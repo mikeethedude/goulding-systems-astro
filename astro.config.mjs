2b8606c5ba639fdcb6bf83aa235920b3467d89a8
Mike Goulding <mikeegoulding@gmail.com>
2024-07-15 17:23:10 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mikeethedude.github.io',
-    base: 'goulding-systems-astro'
+    site: 'https://gouldingcreations.com',
 });
